cd7c5b96b4ede537fdc3400df68fbba345b44d0f
LyzzHans <lyzz.hans@gmail.com>
2021-12-26 20:36:16 -0500

    hashtag repo and model

--- src/main/java/org/wecancodeit/com/project/repositories/ContinentRepository.java
+++ src/main/java/org/wecancodeit/com/project/repositories/ContinentRepository.java
@@ -1,11 +1,12 @@
 package org.wecancodeit.com.project.repositories;
-public interface ContinentRepository {
+
 
 
 import org.springframework.data.repository.CrudRepository;
 import org.wecancodeit.com.project.models.Continent;
 
 public interface ContinentRepository extends CrudRepository<Continent, Long> {
+    Continent findContinentByName(String name);
 
 }
 
--- src/main/java/org/wecancodeit/com/project/repositories/CountryRepository.java
+++ src/main/java/org/wecancodeit/com/project/repositories/CountryRepository.java
@@ -5,9 +5,6 @@ import org.wecancodeit.com.project.models.Country;
 
 public interface CountryRepository extends CrudRepository<Country, Long> {
 
-=======import java.util.Collection;
-
-public interface CountryRepository {
-    Collection<IslandInfo> getIslands();
+    Country findCountryByContinent(String continent);
 
 }
