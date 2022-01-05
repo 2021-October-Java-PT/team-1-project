66dc0f1f4e5a4d731ab085c59e64ed3f033fd70b
TreyBrowder <tbrowder7@ymail.com>
2022-01-05 12:30:53 -0500

    added all missing data, and added in photos needed with data

--- src/main/java/org/wecancodeit/com/project/Populator.java
+++ src/main/java/org/wecancodeit/com/project/Populator.java
@@ -27,12 +27,12 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Continent northAmerica = new Continent("North America", "image Url here");
-        Continent SouthAmerica = new Continent("South America", "image Url here");
-        Continent Africa = new Continent("Africa", "image Url here");
-        Continent Asia = new Continent("Asia", "image Url here");
-        Continent Europe = new Continent("Europe", "image Url here");
-        Continent Australia = new Continent("Australia", "image Url here");
+        Continent northAmerica = new Continent("North America", "/static/images/northAmerica.jpg");
+        Continent SouthAmerica = new Continent("South America", "/static/images/southAmerica.jpg");
+        Continent Africa = new Continent("Africa", "/static/images/africa.jpg");
+        Continent Asia = new Continent("Asia", "/static/images/asia.jpg");
+        Continent Europe = new Continent("Europe", "/static/images/europe.jpg");
+        Continent Australia = new Continent("Australia", "/static/images/australia.jpg");
         continentRepo.save(northAmerica);
         continentRepo.save(SouthAmerica);
         continentRepo.save(Africa);
@@ -41,24 +41,24 @@ public class Populator implements CommandLineRunner {
         continentRepo.save(Australia);
 
         Country republicFiji = new Country( "Republic of Fiji", "/images/world-map.gif");
-        Country italy = new Country("italy", "image url here");
-        Country usa = new Country("United States of America", "image Url here");
-        Country venezuela = new Country("Venezuela", "image Url here");
-        Country caboVerde = new Country("Cabo Verde", "image url here");
-        Country thailand = new Country("Thailand", "image url here");
+        Country italy = new Country("italy", "/static/images/Italy.jpg");
+        Country usa = new Country("United States of America", "/static/images/usa.jpg");
+        Country venezuela = new Country("Venezuela", "/static/images/venezuela.jpg");
+        Country capeVerde = new Country("Cape Verde", "/static/images/CapeVerde.jpg");
+        Country thailand = new Country("Thailand", "/static/images/Thailand.jpg");
         countryRepo.save(republicFiji);
         countryRepo.save(italy);
         countryRepo.save(usa);
         countryRepo.save(venezuela);
-        countryRepo.save(caboVerde);
+        countryRepo.save(capeVerde);
         countryRepo.save(thailand);
 
-        Island fiji = new Island("Fiji", "image url here", "Australia", "Republic of Fiji", "South Pacific Ocean", "decript here");
-        Island levanzo = new Island("Levanzo", "image url here", "Europe", "Italy", "Mediterranean Sea", "decript here");
-        Island maui = new Island("Maui", "image url here", "North America", "US", "Central Pacific", "decript here");
-        Island laTortuga = new Island("LaTortuga", "image url here", "South America", "Venezuela", "Caribbean Sea", "decript here");
-        Island santiagoIsland = new Island("Santiago Island", "image url here", "Africa", "Cabo Verde", "Central Atlantic", "decript here");
-        Island similanIslands = new Island("Similan Islands ", "image url here", "Asia", "Thailand", "Caribbean Sea", "decript here");
+        Island fiji = new Island("Fiji", "/static/images/Fiji.jpg", "Australia", "Republic of Fiji", "South Pacific Ocean", "Fiji is a Melanesian country in the south pacific Ocean.  Its made up about 332 islands.  The climiate is mostly tropical with only slight variations throughout the seasons (Possible chance of tropical cyclonic storms from November through April).");
+        Island levanzo = new Island("Levanzo", "/static/images/levanzoIsland.jpg", "Europe", "Italy", "Mediterranean Sea", "Levanzo is one of the most beautiful islands in Italy.  Its surrounded by the blue green waters of the Mediterranean Sea.  The island is also characterized by the white limestone rocks and many caves to visit.  There is plenty to do ranging from trekking, beaches, diving, boat tours etc.");
+        Island maui = new Island("Maui", "/static/images/mauiIsland.jpg", "North America", "US", "Central Pacific", "Maui, known also as “The Valley Isle,” is the second largest Hawaiian island. The island beloved for its world-famous beaches, the sacred Iao Valley, views of migrating humpback whales (during winter months), farm-to-table cuisine and the magnificent sunrise and sunset from Haleakala.");
+        Island laTortuga = new Island("LaTortuga", "/static/images/LaTortuga.jpg", "South America", "Venezuela", "Caribbean Sea", "La Tortuga Island (Spanish: Isla La Tortuga; \"La Tortuga\" means \"the turtle\") is an uninhabited island of Venezuela, the largest in the Federal Dependencies of Venezuela. It is part of a group of islands that include the Tortuguillos and Cayo Herradura");
+        Island santiagoIsland = new Island("Santiago Island", "/static/images/santiago.jpg", "Africa", "Cape Verde", "Central Atlantic", "This island consist of two overlapping volcanoes.  Common wildlife that can be seen on or around the island are: fur seals, sea lions, lightfoot crabs, marine iguana, dolphins, rice rats, and sharks.");
+        Island similanIslands = new Island("Similan Islands ", "/static/images/similan.jpg", "Asia", "Thailand", "Caribbean Sea", "The Similan Islands are an archipelago of 11 islands in the Andaman Sea 70 kilometers off the coast of southern Thailand's Phang Nga Province, the Similan Islands. The island is best known as a diving and snorkeling destination, but the Similans' scenic moorings have also become popular with the sailing fraternity.");
         islandRepo.save(fiji);
         islandRepo.save(levanzo);
         islandRepo.save(maui);
